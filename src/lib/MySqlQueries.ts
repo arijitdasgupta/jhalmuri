e30881bb576ed0ee3d476bb611381ef03a824a0b
Arijit Dasgupta <arijit.rivalslayer@gmail.com>
2017-02-18 01:11:29 +0100

    Some changes

--- src/lib/MySqlQueries.ts
+++ src/lib/MySqlQueries.ts
@@ -4,17 +4,34 @@ import { IPost } from '../interfaces/IPost';
 export class MySqlQueries {
   constructor(private pool:MySqlPool) {}
 
-  getAllPosts = () => {
-    return new Promise<IPost[]>((resolve, reject) => {
-      this.pool.connectionPool.getConnection(function (err, connection) {
+  private getConnection = () => {
+    return new Promise((resolve, reject) => {
+        this.pool.connectionPool.getConnection(function (err, connection) {
           if (!err) {
-              connection.query(`select * from wp_posts where post_status='publish'`, function (err, results) {
-                  connection.release();
-                  resolve(results as IPost[]);
-              });
+            resolve(connection);
           } else {
             reject(err);
           }
+        });
+    });
+  }
+
+  // Make this RxJS compatible
+  getAllPosts = () => {
+    return new Promise<IPost[]>((resolve, reject) => {
+      this.getConnection.then((connection) => {
+        connection.query(`select * from wp_posts where post_status='publish'`, function (err, results) {
+            connection.release();
+            resolve(results as IPost[]);
+        });
+      });
+    });
+  }
+
+  getSiteData = () => {
+    return new Promise((resolve, reject) => {
+      this.pool.connectionPool.getConnection(function (err, connection) {
+
       });
     });
   }
